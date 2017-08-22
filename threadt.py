decb6f4b5f42fb87a9b4edc212743b519c238630
alexwan02 <alexwan0312@gmail.com>
2017-08-22 14:53:08 +0800

    thread & process

---
+++ threadt.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import time , threading 
+
+def loop():
+	print 'thread %s is running...' % threading.current_thread().name
+	n = 0
+
+	while n < 5:
+		n = n + 1
+		print 'thread %s >>> %s' % (threading.current_thread().name , n)
+		time.sleep(1)
+
+	print 'thread %s ended.' % threading.current_thread().name
+
+print 'thread %s is running...' % threading.current_thread().name
+
+# t = threading.Thread(target = loop , name = 'LoopThread')
+
+# t.start()
+
+# t.join()
+
+print 'thread %s ended.' % threading.current_thread().name
+
+
+# multithread
+balance = 0
+
+lock = threading.Lock()
+
+def chanfe_it(n):
+	global balance
+	balance = balance + n
+	balance = balance - n
+
+def run_thread(n):
+	for i in range(100000):
+		lock.acquire()
+		try:
+			chanfe_it(n)
+		finally: 
+			lock.release()
+
+t1 = threading.Thread(target = run_thread , args=(5,))
+t2 = threading.Thread(target = run_thread , args= (8,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print balance
+
+# threadlocal 
+
+local_school = threading.local()
+
+def process_student():
+	print 'Hello , %s (in %s)' % (local_school.student , threading.current_thread().name)
+
+def process_thread(name):
+	local_school.student = name
+	process_student()
+
+t3 = threading.Thread(target = process_thread , args=('Alex' ,) , name='Thread A')
+t4 = threading.Thread(target = process_thread , args=('Lucky' ,) , name='Thread B')
+
+t3.start()
+t4.start()
+
+t3.join()
+t4.join()
+
+
+
+
+
